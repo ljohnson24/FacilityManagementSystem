be27362f1526e59c51039c86941db94cc01c82eb
Shaun <ljohnson24@luc.edu>
2018-02-20 01:18:53 -0600

    IFacilityschedule changes

--- src/com/fms/model/facilityuse/IFacilitySchedule.java
+++ src/com/fms/model/facilityuse/IFacilitySchedule.java
@@ -1,5 +1,11 @@
 package com.fms.model.facilityuse;
 
-public interface IFacilitySchedule {
+import com.fms.model.facility.IRoom;
 
+public interface IFacilitySchedule {
+	void assignFacilityToUse(IRoom room, IUser user);
+	void vacateFacility(IRoom room);
+	boolean isInUseDuringInterval(IRoom room, int interval);
+	double calcUsageRate(IRoom room);
+	double listActualUsage(IRoom room);
 }
--- src/com/fms/model/facilityuse/IFacilityUse.java
+++ src/com/fms/model/facilityuse/IFacilityUse.java
@@ -11,4 +11,9 @@ public interface IFacilityUse {
 	void addInspection(IInspection inspection);
 	void removeInspection(IInspection inspection);
 	List<IInspection> listInspection();
+	void assignFacilityToUse(IRoom room, IUser user);
+	void vacateFacility(IRoom room);
+	boolean isInUseDuringInterval(IRoom room, int interval);
+	double calcUsageRate(IRoom room);
+	double listActualUsage(IRoom room);
 }
