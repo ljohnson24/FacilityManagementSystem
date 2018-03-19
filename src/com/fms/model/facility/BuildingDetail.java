20e398de0dc0f715a799cd8517c7a8f9dfe0b417
Shaun <ljohnson24@luc.edu>
2018-03-19 00:17:31 -0500

    other changes

---
+++ src/com/fms/model/facility/BuildingDetail.java
@@ -0,0 +1,44 @@
+package com.fms.model.facility;
+
+public class BuildingDetail implements IBuildingDetail {
+	private String buildingname;
+	private String buildingaddress;
+	private int buildingphone;
+
+	
+	public String getBuildingName() {
+		
+		return this.buildingname;
+	}
+
+	
+	public void setBuildingName(String buildingname) {
+		this.buildingname = buildingname;
+
+	}
+
+	
+	public String getBuildingAddress() {
+		
+		return  this.buildingaddress;
+	}
+
+	
+	public void setBuildingAddress(String buildingaddress) {
+		this.buildingaddress = buildingaddress;
+
+	}
+
+	
+	public int getBuildingPhone() {
+		
+		return this.buildingphone;
+	}
+
+	
+	public void setBuildingPhone(int buildingphone) {
+		this.buildingphone = buildingphone;
+
+	}
+
+}
---
+++ src/com/fms/model/facility/IBuildingDetail.java
@@ -0,0 +1,12 @@
+package com.fms.model.facility;
+
+public interface IBuildingDetail {
+	
+	String getBuildingName();
+	void setBuildingName(String buildingname);
+	String getBuildingAddress();
+	void setBuildingAddress(String buildingaddress);
+	int getBuildingPhone();
+	void setBuildingPhone(int buildingphone);
+	
+}
