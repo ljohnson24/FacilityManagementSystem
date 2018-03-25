4e7725aa0cff70f83e70dc65cefcf0fc31f7887f
Shaun <ljohnson24@luc.edu>
2018-03-25 14:57:26 -0500

    added unit test skeleton for facilitytest

---
+++ src/com/fms/test/facility/BuildingDetailTest.java
@@ -0,0 +1,14 @@
+package com.fms.test.facility;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BuildingDetailTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/com/fms/test/facility/FacilityFilterTest.java
@@ -0,0 +1,14 @@
+package com.fms.test.facility;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FacilityFilterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
