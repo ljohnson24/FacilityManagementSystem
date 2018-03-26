2d0c7e9e79b8fed4589620bc2e38c75d67996e1a
Shaun <ljohnson24@luc.edu>
2018-03-26 00:26:17 -0500

    need to add finish sort implementation

--- src/com/fms/model/facility/FacilitySort.java
+++ src/com/fms/model/facility/FacilitySort.java
@@ -1,11 +1,19 @@
 package com.fms.model.facility;
 
 import java.util.List;
+import java.util.Comparator;
+import java.util.Collections;
+import java.util.ArrayList;
 public class FacilitySort implements IFacilitySort {
 	
 	private List<IBuilding> buildings;
+	private List<IBuildingDetail> buildingdetail;
 	
 	public List<IBuilding> sortByName(List<IBuilding> buildings){
+		this.buildings.clear();
+		for (IBuilding building:buildings) {
+			
+		}
 		return this.buildings;
 	}
 
