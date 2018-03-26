13973506f5724dc550f0c2b99a0120c4c88d40dc
Shaun <ljohnson24@luc.edu>
2018-03-25 23:54:38 -0500

    facilityfilter implementation completed

--- src/com/fms/model/facility/FacilityFilter.java
+++ src/com/fms/model/facility/FacilityFilter.java
@@ -1,39 +1,44 @@
 package com.fms.model.facility;
 
+import com.fms.model.facility.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class FacilityFilter implements IFacilityFilter {
-	
 	private List<IBuilding> buildings;
 	
-	private List<IBuilding> getBuildings() {
+	public FacilityFilter() {
+		this.buildings = new ArrayList<>();
+	}
+
+	public List<IBuilding> filterByOccupancy(List<IBuilding> buildings) {
+		this.buildings.clear();
+		for (IBuilding building:buildings) {
+			for (IRoom room: building.getRooms()) {
+				if (room.getRoomStatus() == true) {
+					this.buildings.add(building);
+					break;
+				}
+			}
+		}
+		
 		return this.buildings;
 	}
-	
-	private void setBuildings(List<IBuilding> buildings) {
-		this.buildings = buildings;
-	}
-	
-	private void addBuilding(IBuilding building) {
-		
-		this.buildings.add(building);
-	}
-	
-	
-	private void clearBuildings() {
-		this.buildings.clear();
-	}
 
-	@Override
-	public List<IBuilding> filterByOccupancy(List<IBuilding> buildings) {
-		
-		return buildings;
-	}
-
-	@Override
 	public List<IBuilding> filterByVacancy(List<IBuilding> buildings) {
 		
-		return buildings;
+		this.buildings.clear();
+		for (IBuilding building:buildings) {
+			for (IRoom room: building.getRooms()) {
+				if (room.getRoomStatus() == false) {
+					this.buildings.add(building);
+					break;
+				}
+			}
+		}
+		
+		return this.buildings;
 	}
 
 }
