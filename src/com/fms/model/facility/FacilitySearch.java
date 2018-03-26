7aa51f25a5f0cdc2da7fa0110dbd814df3c93d45
Shaun <ljohnson24@luc.edu>
2018-03-25 22:20:26 -0500

    facilitysearch implementation completed

--- src/com/fms/model/facility/FacilitySearch.java
+++ src/com/fms/model/facility/FacilitySearch.java
@@ -1,20 +1,30 @@
 package com.fms.model.facility;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class FacilitySearch implements IFacilitySearch {
-	
-	private List<IBuilding> buildings;
 
-	@Override
 	public IBuilding searchByID(List<IBuilding> buildings, String buildingid) {
-		
+		ListIterator<IBuilding> iter = buildings.listIterator();
+		while(iter.hasNext()) {
+			if (iter.next().getBuildingID().equals(buildingid)) {
+				return iter.next();
+			}
+		}
 		return null;
 	}
 
-	@Override
+	
 	public IBuilding searchByName(List<IBuilding> buildings, String buildingname) {
 		
+		ListIterator<IBuilding> iter = buildings.listIterator();
+		while(iter.hasNext()) {
+			if (iter.next().getBuildingDetail().getBuildingName().equals(buildingname)) {
+				return iter.next();
+			}
+		}
 		return null;
 	}
 
